27997d930661af6e0ff96d4f86f01441ad931294
Michael J. MacMullin <mike.macmullin@gmail.com>
2019-08-27 14:20:33 -0230

    Update nano.rb

--- nano.rb
+++ nano.rb
@@ -1,32 +1,18 @@
+require 'formula'
+
 class Nano < Formula
-  desc "A free (GNU) replacement for the Pico text editor"
-  homepage "http://www.nano-editor.org/"
-  url "http://www.nano-editor.org/dist/v2.4/nano-2.4.2.tar.gz"
-  sha256 "c8cd7f18fcf5696d9df3364ee2a840e0ab7b6bdbd22abf850bbdc951db7f65b9"
-  revision 1
-
-  bottle do
-    cellar :any
-    sha256 "f282672a102b5b9f1034c3ee46792394b341dce14856dc6746cb56ae15eda6f2" => :yosemite
-    sha256 "9803757b95a170d6bcc1ca7a60a78b2e5c964d0d5af17e700b8aafcf3d9a9528" => :mavericks
-    sha256 "25623a18d91f5325386e2e25cab2137e54a5d02b71657931024c731916dcf58a" => :mountain_lion
-  end
-
-  head do
-    url "svn://svn.sv.gnu.org/nano/trunk/nano"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-  end
+  homepage 'http://www.nano-editor.org/'
+  url 'http://www.nano-editor.org/dist/v2.4/nano-2.4.0.tar.gz'
+  sha1 '55639cbac2d38febf16780b912b036f2023534d1'
+  head 'svn://svn.sv.gnu.org/nano/trunk/nano'
+  # see https://savannah.gnu.org/bugs/?44609
+  patch :DATA
 
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "homebrew/dupes/ncurses"
 
   def install
-    # Otherwise SIGWINCH will not be defined
-    ENV.append_to_cflags "-U_XOPEN_SOURCE" if MacOS.version < :leopard
-
-    system "./autogen.sh" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -37,10 +23,23 @@ class Nano < Formula
                           "--enable-nanorc",
                           "--disable-nls",
                           "--enable-utf8"
-    system "make", "install"
+    system "make install"
   end
 
   test do
     system "#{bin}/nano", "--version"
   end
 end
+
+__END__
+--- a/src/text.c	2015-03-22 23:45:12.000000000 -0400
++++ b/src/text.c	2015-03-24 22:44:05.000000000 -0400
+@@ -2664,7 +2664,7 @@ const char *do_alt_speller(char *tempfil
+     ssize_t current_y_save = openfile->current_y;
+     ssize_t lineno_save = openfile->current->lineno;
+     struct stat spellfileinfo;
+-    __time_t timestamp;
++    time_t timestamp;
+     pid_t pid_spell;
+     char *ptr;
+     static int arglen = 3;
